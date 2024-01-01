3af8c65120d3c405eac3e948443f4c746fc36730
Emil <emiliano.milewski@gmail.com>
2024-01-01 14:08:16 +0000

    day4 file setup

---
+++ Day4/index.js
@@ -0,0 +1,13 @@
+import { readFileSync } from "fs";
+
+function getFileData(fileLocation) {
+    const fileData = readFileSync(fileLocation, "utf8", (err, data) => {
+        if(err) throw err; 
+        return data;
+    });
+    return fileData;
+};
+const inputLocation = "./Day3/input.txt";
+const inputData = getFileData(inputLocation);
+
+const inputArray = inputData.split("\n");
