f38366ba640ad6ee03efd9ff19cd55c1658020ab
Emil <emiliano.milewski@gmail.com>
2024-01-01 15:10:30 +0000

    day4 part 1 complete

--- Day4/index.js
+++ Day4/index.js
@@ -7,7 +7,33 @@ function getFileData(fileLocation) {
     });
     return fileData;
 };
-const inputLocation = "./Day3/input.txt";
+const inputLocation = "./Day4/input.txt";
 const inputData = getFileData(inputLocation);
 
 const inputArray = inputData.split("\n");
+
+let answer = 0;
+
+for (let i = 0; i < inputArray.length; i++) {
+    const [,numData] = inputArray[i].split(":");
+    let [winningNums, nums] = numData.split("|");
+
+    winningNums = winningNums.trim().split(" ").filter((item) => Number.isInteger(parseInt(item)));
+    nums = nums.trim().split(" ").filter((item) => Number.isInteger(parseInt(item)));
+
+    let sum = 0;
+    for(let j = 0; j < nums.length; j++) {
+        for(let x = 0; x < winningNums.length; x++) {
+            if (winningNums[x] === nums[j]) {
+                if (sum === 0) {
+                    sum += 1;
+                } else {
+                    sum *= 2;
+                };
+            };
+        };
+    };
+    answer += sum;
+};
+
+console.log(answer);
