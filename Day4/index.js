c7fd8fadd5ef7c91a3dfab2c52d84e01f16e5656
Emil <emiliano.milewski@gmail.com>
2024-01-02 22:13:40 +0000

    finished part 2 of day4

--- Day4/index.js
+++ Day4/index.js
@@ -13,11 +13,19 @@ const inputData = getFileData(inputLocation);
 const inputArray = inputData.split("\n");
 
 let answer = 0;
+let cards = {};
 
 for (let i = 0; i < inputArray.length; i++) {
-    const [,numData] = inputArray[i].split(":");
-    let [winningNums, nums] = numData.split("|");
+    const [cardNumData,] = inputArray[i].split(":");
+    const cardNum = cardNumData.trim().split(" ").filter((item) => item !== "").join("-");
+    cards[cardNum] = 1;
+};
 
+for (let i = 0; i < inputArray.length; i++) {
+    const [cardNumData, numData] = inputArray[i].split(":");
+    const cardNum = cardNumData.trim().split(" ").filter((item) => item !== "").join("-");
+
+    let [winningNums, nums] = numData.split("|");
     winningNums = winningNums.trim().split(" ").filter((item) => Number.isInteger(parseInt(item)));
     nums = nums.trim().split(" ").filter((item) => Number.isInteger(parseInt(item)));
 
@@ -25,15 +33,24 @@ for (let i = 0; i < inputArray.length; i++) {
     for(let j = 0; j < nums.length; j++) {
         for(let x = 0; x < winningNums.length; x++) {
             if (winningNums[x] === nums[j]) {
-                if (sum === 0) {
-                    sum += 1;
-                } else {
-                    sum *= 2;
-                };
+                sum += 1;
             };
         };
     };
-    answer += sum;
+
+    sum *= 1;
+    for(let j = 1; j <= sum; j++) {
+        let [cardText,idNum] = cardNum.split("-");
+        idNum = parseInt(idNum);
+        const nextCard = [cardText, idNum+j].join("-"); 
+        if (cards.hasOwnProperty(nextCard) === false) break;
+        cards[nextCard] += cards[cardNum];
+    };
 };
 
+for (const card in cards) {
+    answer += cards[card];
+};
+
+console.log(cards);
 console.log(answer);
