93f1bbab7dededf2c3fa2e2c3be1447f474461d9
Emil <emiliano.milewski@gmail.com>
2024-01-03 23:27:17 +0000

    day6 file setup

---
+++ Day6/index.js
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+
+function getFileData(fileLocation) {
+    const fileData = readFileSync(fileLocation, "utf8", (err, data) => {
+        if(err) throw err; 
+        return data;
+    });
+    return fileData;
+};
+const inputLocation = "./Day5/input.txt";
+const inputData = getFileData(inputLocation);
+
+const inputArray = inputData;
+console.log(inputArray);
